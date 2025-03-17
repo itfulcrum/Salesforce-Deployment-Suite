5e169afac95a94d0569f87bc8b102cb7f2023aa3
ramesh.g <ramesh.g@cloudfulcrum.com>
2025-03-17 10:40:27 +0530

    updated required files.

--- cf-sf-secure-quality-gate/entrypoint.sh
+++ cf-sf-secure-quality-gate/entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-echo "Running Salesforce Delta Package Creation"
-java -cp /app/sfdxlean.jar com.cf.services.PackageCreation \
-  -c="$1" \
-  -s="$2" \
-  -e="$3" \
-  -f="$4" \
-  -a="$5" \
-  -x=package
+echo "Running Salesforce Code Analyzer"
+sf scanner:run --format csv --target './**/*.cls' --category "Design,Best Practices,Performance" --outfile ${{ github.workspace }}/dist/'ScanResults.csv'
